be132eceaffb6eb6abeb19604b9246c0ac475d50
cooperdff <87905806+cooperdff@users.noreply.github.com>
2021-08-02 17:51:29 -0500

    v0.1.4 publish and associated files

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/cooperdff/nfl_data_py'
 EMAIL = 'cooper.dff11@gmail.com'
 AUTHOR = 'cooperdff'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.3'
+VERSION = '0.1.4'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
